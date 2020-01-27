c3f193384a4a6f82149787fd72f837b24909c164
regrep2 <59843048+regrep2-1730268@users.noreply.github.com>
2020-01-26 16:49:48 -0800

    Add files via upload

---
+++ lavaapp.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+            body{background-color:rgb(255, 0, 0);}
+            h1{color:black;text-align:center;Font-family:arial;font-size:30px}
+        </style>
+    </head>
+    <body>
+        <h1>Thank you for applying to the LAVA Lamp Company! We will review you application within 30 days. Have a lavalicious day!</h1>
+    </body>
+</html>
